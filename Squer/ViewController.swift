e2b5e95c54ca9e45a79a2bf418609f6b4022638c
Birewkoss <birewkoff@gmail.com>
2024-05-02 14:49:37 +0300

    Initial Commit

---
+++ Squer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Squer
+//
+//  Created by Артур Бирюков on 02.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
